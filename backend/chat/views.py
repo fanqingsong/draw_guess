6af4ce674a2cd369630f3de23044dbab924590ef
qsfan <qsfan@qq.com>
2021-11-11 17:28:39 +0800

    query comments by drawing id. OK

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Drawings, Comments
 from .serializer import DrawingsSerializer, CommentsSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.shortcuts import get_list_or_404
 
 def index(request):
     return render(request, 'chat/index.html')
@@ -24,3 +27,21 @@ class CommentViewSet(viewsets.ModelViewSet):
     """
     queryset = Comments.objects.all()
     serializer_class = CommentsSerializer
+
+    @action(methods=['get'], detail=False)
+    def query(self, request):
+        print("-------------------")
+        print(request.query_params)
+
+        drawing = request.query_params["drawing"]
+        print("=====")
+        print(drawing)
+
+        queryset = Comments.objects.filter(drawing=drawing).order_by('id')
+        
+        # target = get_list_or_404(queryset)
+        serializer = CommentsSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+
